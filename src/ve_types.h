c16eaf2a0da55cce5185a0b2a6b3fb5834495a60
vadimvinokurov <vad9fly@gmail.com>
2022-02-17 22:04:28 +0300

    add core header which includes engine's base headers

---
+++ src/ve_types.h
@@ -0,0 +1,13 @@
+//
+// Created by boris on 2/17/2022.
+//
+
+#ifndef VENGINE3D_VE_TYPES_H
+#define VENGINE3D_VE_TYPES_H
+
+typedef uint32_t uint32;
+typedef int32_t int32;
+typedef uint64_t uint64;
+typedef int64_t int64;
+
+#endif //VENGINE3D_VE_TYPES_H
