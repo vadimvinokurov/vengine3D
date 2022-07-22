c74251750f7a40bd1421b7a5ec6d3c95ce500ef0
vadimvinokurov <vad9fly@gmail.com>
2022-07-23 02:08:18 +0300

    Added control of the sequence of pointer when deallocating

--- Sources/ECS/Memory/StackAllocator.cpp
+++ Sources/ECS/Memory/StackAllocator.cpp
@@ -7,9 +7,63 @@
 
 StackAllocator::StackAllocator(void *memory, size_t size) : IAllocator(memory, size)
 {
+	issuedMemory_.reserve(10);
+	freedMemory_.reserve(10);
 }
 
 void *StackAllocator::allocate(size_t size, uint8 alignment)
+{
+	auto ptr = allocate_memory(size, alignment);
+	if (ptr)
+	{
+		issuedMemory_.push_back(ptr);
+	}
+
+	return ptr;
+}
+
+void StackAllocator::deallocate(void *ptr)
+{
+	if (!ptr)
+	{
+		return;
+	}
+	assert(std::find(issuedMemory_.begin(), issuedMemory_.end(), ptr) != issuedMemory_.end());
+	if (ptr == issuedMemory_.back())
+	{
+		deallocate_memory(ptr);
+		issuedMemory_.pop_back();
+
+		bool check = true;
+		while (check)
+		{
+			check = false;
+			for (auto &freedPtr : freedMemory_)
+			{
+				if (freedPtr == issuedMemory_.back())
+				{
+					deallocate_memory(freedPtr);
+					issuedMemory_.pop_back();
+					std::swap(freedPtr, freedMemory_.back());
+					freedMemory_.pop_back();
+					check = true;
+					break;
+				}
+			}
+		}
+	}
+	else
+	{
+		freedMemory_.push_back(ptr);
+	}
+}
+
+void StackAllocator::clear()
+{
+	usedMemory_ = 0;
+}
+
+void *StackAllocator::allocate_memory(size_t size, uint8 alignment)
 {
 	assert(size > 0 && "allocate called with memSize = 0.");
 
@@ -31,11 +85,11 @@ void *StackAllocator::allocate(size_t size, uint8 alignment)
 	meta->adjustment = adjustment;
 
 	usedMemory_ += size + adjustment;
-
+	memset(asVoid, 0xFF, size);
 	return asVoid;
 }
 
-void StackAllocator::deallocate(void *ptr)
+void StackAllocator::deallocate_memory(void *ptr)
 {
 	assert(usedMemory_ > 0 && "Memory already free.");
 
@@ -50,9 +104,6 @@ void StackAllocator::deallocate(void *ptr)
 
 	auto freedMemorySize = ((uptr)baseAddress_ + usedMemory_) - asUptr;
 	usedMemory_ -= freedMemorySize;
-}
 
-void StackAllocator::clear()
-{
-	usedMemory_ = 0;
+	memset(asVoid, 0x00, freedMemorySize);
 }
--- Sources/ECS/Memory/StackAllocator.h
+++ Sources/ECS/Memory/StackAllocator.h
@@ -6,6 +6,7 @@
 #define VENGINE3D_STACKALLOCATOR_H
 
 #include "IAllocator.h"
+#include <vector>
 
 class StackAllocator : public IAllocator
 {
@@ -20,6 +21,12 @@ public:
 	void *allocate(size_t size, uint8 alignment) override;
 	void deallocate(void *ptr) override;
 	void clear() override;
+private:
+	void *allocate_memory(size_t size, uint8 alignment);
+	void deallocate_memory(void *ptr);
+
+	std::vector<void *> issuedMemory_;
+	std::vector<void *> freedMemory_;
 };
 
 #endif // VENGINE3D_STACKALLOCATOR_H
