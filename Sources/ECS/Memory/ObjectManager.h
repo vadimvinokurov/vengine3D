e7e26d717383c8c96d4763e590abf26bb9aaf0be
vadimvinokurov <vad9fly@gmail.com>
2022-08-05 00:20:05 +0300

    Add iterator to ObjectManager

--- Sources/ECS/Memory/ObjectManager.h
+++ Sources/ECS/Memory/ObjectManager.h
@@ -13,8 +13,11 @@ template <typename T, size_t MAX_CHUNK_SIZE>
 class ObjectManager
 {
 private:
-	using Allocator = BlockAllocator<sizeof(T), alignof(T)>;
 	static constexpr auto CHUNK_MEMORY_SIZE = MAX_CHUNK_SIZE * (sizeof(T) + alignof(T));
+
+	using Allocator = BlockAllocator<sizeof(T), alignof(T)>;
+	using ObjectList = std::vector<T *>;
+
 	struct Chunk
 	{
 		explicit Chunk(const std::shared_ptr<Allocator> &alloc) : allocator(alloc)
@@ -27,10 +30,66 @@ private:
 		Chunk *operator=(Chunk &&other) = delete;
 
 		std::shared_ptr<Allocator> allocator;
-		std::unordered_set<T *> objects;
+		ObjectList objects;
 	};
+	using MemoryChunks = std::list<Chunk>;
 
 public:
+	class iterator : public std::iterator<std::forward_iterator_tag, T>
+	{
+	public:
+		iterator(typename MemoryChunks::iterator begin, typename MemoryChunks::iterator end)
+			: currentChunk_(begin), end_(end)
+		{
+			if (begin != end)
+			{
+				currentObject_ = currentChunk_->objects.begin();
+			}
+			else
+			{
+				currentObject_ = std::prev(end_)->objects.end();
+			}
+		}
+
+		iterator &operator++()
+		{
+			++currentObject_;
+			if (currentObject_ == currentChunk_->objects.end())
+			{
+				++currentChunk_;
+				if (currentChunk_ != end_)
+				{
+					currentObject_ = currentChunk_->objects.begin();
+				}
+			}
+			return *this;
+		}
+
+		T &operator*() const
+		{
+			return *(*currentObject_);
+		}
+
+		T *operator->() const
+		{
+			return *currentObject_;
+		}
+
+		bool operator==(iterator &other)
+		{
+			return ((this->currentChunk_ == other.currentChunk_) && (this->currentObject_ == other.currentObject_));
+		}
+		bool operator!=(iterator &other)
+		{
+			return ((this->currentChunk_ != other.currentChunk_) && (this->currentObject_ != other.currentObject_));
+		}
+
+	private:
+		typename MemoryChunks::iterator currentChunk_;
+		typename MemoryChunks::iterator end_;
+		typename ObjectList::iterator currentObject_;
+	};
+
 	ObjectManager() = default;
 
 	template <typename... Args>
@@ -43,7 +102,7 @@ public:
 			{
 				continue;
 			}
-			chunk.objects.emplace((T *)ptr);
+			chunk.objects.push_back((T *)ptr);
 			new (ptr) T(std::forward<Args>(args)...);
 			return (T *)ptr;
 		}
@@ -54,7 +113,7 @@ public:
 		auto ptr = chunk.allocator->allocate();
 
 		assert(ptr != nullptr && "Object manager can't allocate memory. Out of memory!");
-		chunk.objects.emplace((T *)ptr);
+		chunk.objects.push_back((T *)ptr);
 		new (ptr) T(std::forward<Args>(args)...);
 		return (T *)ptr;
 	}
@@ -73,10 +132,21 @@ public:
 		}
 	}
 
-	~ObjectManager() {
+	iterator begin()
+	{
+		return iterator(chunks_.begin(), chunks_.end());
+	}
+	iterator end()
+	{
+		return iterator(chunks_.end(), chunks_.end());
+	}
+
+	~ObjectManager()
+	{
 		for (auto &chunk : chunks_)
 		{
-			for(auto ptr: chunk.objects) {
+			for (auto ptr : chunk.objects)
+			{
 				ptr->~T();
 				chunk.allocator->free((void *)ptr);
 			}
